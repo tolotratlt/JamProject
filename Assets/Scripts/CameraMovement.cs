ec09994fab1394ddd7ef2c8979207f67adae5e30
ElSombrero2 <nirilala15@gmail.com>
2021-02-13 15:48:08 +0300

    End the first level

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CameraMovement : MonoBehaviour
 {
@@ -13,12 +14,6 @@ public class CameraMovement : MonoBehaviour
     {
         QualitySettings.vSyncCount = 1;
         Application.targetFrameRate = 60;
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
     }
 
     // Update is called once per frame
---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Awake()
+    {
+        Application.targetFrameRate = 30;
+    }
+
+    // Start is called before the first frame update
+    public void LoadNewGame()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void Credits()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
